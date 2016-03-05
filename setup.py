f100254af697b4d852a460407f8f5ba8a8ebee15
T47 <t47@stanford.edu>
2016-03-05 14:30:54 -0800

    add zip_safe flag

--- setup.py
+++ setup.py
@@ -27,5 +27,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5'
-    )
+    ),
+
+    zip_safe=True
 )
