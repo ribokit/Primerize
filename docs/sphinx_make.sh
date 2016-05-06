0bc5c7855a1bbb0a15599474b32568b2ff24c79e
T47 <t47@stanford.edu>
2016-05-06 16:38:52 -0700

    include sphinx make batch

---
+++ docs/sphinx_make.sh
@@ -0,0 +1,11 @@
+make clean
+make html
+
+cd build/html/_static/
+rm basic.css pygments.css
+rm jquery*.js underscore*.js
+rm ajax-loader.gif comment-bright.png comment-close.png comment.png down-pressed.png down.png file.png minus.png plus.png up-pressed.png up.png
+# rm -rf css/ fonts/ images/ js/
+# rm ribokit.gif
+
+cd ../../..
